49113cabeef83c514c47db735e842ec761be89ab
eclipse78 <132812659+eclipse78@users.noreply.github.com>
2024-03-16 20:37:55 +0530

    Update script.py

--- script.py
+++ script.py
@@ -13,28 +13,22 @@ I'ᴍ Fɪʟᴇs Eɴᴄᴏᴅᴇʀ ʙᴏᴛ ᴄᴀɴ ᴄᴏᴍᴘʀᴇss ʏᴏᴜ
 ❗**Yᴏᴜ ʜᴀsɴ'ᴛ sᴛᴀʀᴛᴇᴅ ᴍᴇ ʏᴇᴛ ᴛᴏ ᴜsᴇ ᴍᴇ ғɪʀsᴛ sᴛᴀʀᴛ ᴍᴇ sᴏ ɪ ᴄᴀɴ ᴡᴏʀᴋ ғʟᴀᴡʟᴇssʟʏ**
 """
     PROGRESS_BAR = """<b>
-╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣
-┣⪼ 🗃️ Sɪᴢᴇ: {1} | {2}
-┣⪼ ⏳️ Dᴏɴᴇ : {0}%
-┣⪼ 🚀 Sᴩᴇᴇᴅ: {3}/s
-┣⪼ ⏰️ Eᴛᴀ: {4}
-╰━━━━━━━━━━━━━━━➣ </b>"""
+┏━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━•
+┣ 🗃️ Sɪᴢᴇ: {1} | {2}
+┣ ⏳️ Dᴏɴᴇ : {0}%
+┣ 🚀 Sᴩᴇᴇᴅ: {3}/s
+┣ ⏰️ Eᴛᴀ: {4}
+┗━━━━━━━━━━━━━━━• </b>"""
 
     SEND_FFMPEG_CODE = """
-❪ SET CUSTOM FFMPEG CODE ❫
+Send me the correct ffmpeg code
 
-Send me the correct ffmpeg code for more info.
+• Format
 
+ffmpeg -i input.mp4 <code> -c:v libx264 -crf 23 </code> output.mp4
 
-☛ <a href=https://unix.stackexchange.com/questions/28803/how-can-i-reduce-a-videos-size-with-ffmpeg#:~:text=ffmpeg%20%2Di%20input.mp4%20%2Dvcodec%20libx265%20%2Dcrf%2028%20output.mp4> FOR HELP </a>
+<code> -c:v libx264 -crf 23 </code> This is your FFMPEG code
 
-⦿ Fᴏʀᴍᴀᴛ Oɴ Hᴏᴡ Tᴏ Sᴇᴛ
-
-☞ ffmpeg -i input.mp4 <code> -c:v libx264 -crf 23 </code> output.mp4
-
-<code> -c:v libx264 -crf 23 </code> Tʜɪs ɪs ʏᴏᴜʀ ғғᴍᴘᴇɢ ᴄᴏᴅᴇ ✅
-
-📥 Fᴏʀ Hᴇʟᴘ Cᴏɴᴛ. @Snowball_Official
 """
 
     SEND_METADATA ="""
@@ -44,34 +38,30 @@ Send me the correct ffmpeg code for more info.
 
 ◦ <code> -map 0 -c:s copy -c:a copy -c:v copy -metadata title="My Video" -metadata author="John Doe" -metadata:s:s title="Subtitle Title" -metadata:s:a title="Audio Title" -metadata:s:v title="Video Title" </code>
 
-📥 Fᴏʀ Hᴇʟᴘ Cᴏɴᴛ. @Snowball_Official
 """
 
     
     HELP_MSG = """
 Available commands:-
 
-➜ /set_ffmpeg - To set custom ffmpeg code
-➜ /set_metadata - To set custom metadata code
-➜ /set_caption - To set custom caption
-➜ /del_ffmpeg - Delete the custom ffmpeg code
-➜ /del_caption - Delete caption
-➜ /see_ffmpeg - View custom ffmpeg code
-➜ /see_metadata - View custom metadata code
-➜ /see_caption - View caption 
-➜ To Set Thumbnail just send photo
+➜ start - Check Bot is Working or not
+➜ set_ffmpeg - Set Your Custom FFMPEG Code
+➜ see_ffmpeg - See Your Custom FFMPEG Code
+➜ del_ffmpeg - Delete Your Current FFMPEG Code
+➜ set_metadata - To set custom metadata code
+➜ see_ffmpeg - View custom ffmpeg code
+➜ set_caption - Set Your Caption
+➜ see_caption - See Your Caption
+➜ del_caption - Delete Your Caption
+➜ del_thumb - Delete Thumbnail
+➜ view_thumb - View Thumbnail
+➜ stats - See Current Status of Bot and No of Users [ADMIN]
+➜ broadcast - Broadcast Message To All The Users [ADMIN]
+➜ ban_user - Ban Perticualr User To Use The Bot [ADMIN]
+➜ unban_user - Unban The User [ADMIN]
+➜ banned_users - List Of Banned Users [ADMIN]
 
 
-<b>⦿ Developer:</b> <a href=https://t.me/Snowball_Official>ѕησωвαℓℓ ❄️</a>
 """
 
-    ABOUT_TXT = """<b>╭───────────⍟
-├🤖 ᴍy ɴᴀᴍᴇ : @{}
-├👨‍💻 Pʀᴏɢʀᴀᴍᴇʀ : <a href=https://t.me/Snowball_Official>𝓢𝓝𝓞𝓦𝓑𝓐𝓛𝓛</a>
-├👑 Instagram : <a href=https://www.instagram.com/ritesh6_>C-Insta</a> 
-├☃️ ꜰᴏᴜɴᴅᴇʀ ᴏꜰ : <a href=https://t.me/+mCdsJ7mjeBEyZWQ1>K-Lᴀɴᴅ</a>
-├📕 Lɪʙʀᴀʀy : <a href=https://github.com/pyrogram>Pyʀᴏɢʀᴀᴍ</a>
-├✏️ Lᴀɴɢᴜᴀɢᴇ: <a href=https://www.python.org>Pyᴛʜᴏɴ 3</a>
-├💾 Dᴀᴛᴀ Bᴀꜱᴇ: <a href=https://cloud.mongodb.com>Mᴏɴɢᴏ DB</a>
-├🌀 ᴍʏ ꜱᴇʀᴠᴇʀ : <a href=https://dashboard.heroku.com>Heroku</a>
-╰───────────────⍟ """
+    ABOUT_TXT = """<b>┏• Creator : <a href='tg://settings'>yours truly</a>\n┣• Channel : <a href='https://t.me/AnimeXWrld'>Anime Wrld</a>\n┗• Support Group : <a href='https://t.me/AnimeXWrld_Chat'>Anime Wrld Chat</a></b> """
